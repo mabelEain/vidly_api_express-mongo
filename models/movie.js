191ddb2f72ce41bc98af49d2d05e0e35c274e33e
mabelEain <mabeleain90@gmail.com>
2022-12-11 17:45:18 +0630

    organize models and routes with customer,genre and movie

---
+++ models/movie.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const {genreSchema} = require('./genere');
+
+const Movie = mongoose.model('Movies', new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    genre: {
+        type: genreSchema,
+        required: true
+    },
+    numberInStock: {
+        type: Number,
+        required: true,
+        minlength: 0,
+        maxlength: 255
+    },
+    dailyRentalRate: {
+        type: Number,
+        required: true,
+        minlength: 0,
+        maxlength: 255
+    }
+}));
+
+function validateMovie(movie) {
+    const schema = {
+      title: Joi.string().min(5).max(50).required(),
+      genreId: Joi.string().required(),
+      numberInStock: Joi.number().min(0).required(),
+      dailyRentalRate: Joi.number().min(0).required()
+    };
+  
+    return Joi.validate(movie, schema);
+  }
+
+  exports.Movie = Movie;
+  exports.validate = validateMovie;
